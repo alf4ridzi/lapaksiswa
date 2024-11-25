46d6d7826aae256087de6bfaa79de3ae70a83a2f
alf4ridzi <aalalfaridzi9@gmail.com>
2024-11-25 18:24:17 +0700

    baru

--- lapaksiswa/app/Controllers/Autentikasi/Autentikasi.php
+++ lapaksiswa/app/Controllers/Autentikasi/Autentikasi.php
@@ -4,11 +4,14 @@ namespace App\Controllers\Autentikasi;
 use App\Controllers\BaseController;
 use App\Models\WebsiteModel;
 use App\Models\KategoriModel;
+use App\Models\UserModel;
+use CodeIgniter\CodeIgniter;
 
 class Autentikasi extends BaseController
 {
-    public function __construct() {
-        
+    public function __construct()
+    {
+        $this->session = session();
     }
 
     public function login(): string
@@ -28,7 +31,8 @@ class Autentikasi extends BaseController
         return view('autentikasi/login', $data);
     }
 
-    public function register() {
+    public function register()
+    {
         $websiteModel = new WebsiteModel();
         $web = $websiteModel->getSettings();
 
@@ -41,5 +45,77 @@ class Autentikasi extends BaseController
         ];
 
         return view('autentikasi/register', $data);
+    }
+
+    public function validasiMasuk()
+    {
+        if ($this->request->getMethod() !== 'post') {
+            throw \CodeIgniter\Exceptions\PageNotFoundException::forPageNotFound();
+        }
+
+        if ($this->session->has('isLogin')) {
+            if ($this->session->get('role') === 'seller') {
+                return redirect()->to('seller');
+            }
+
+            return redirect()->to('/');
+        }
+
+        $userModel = new UserModel();
+        $username = htmlspecialchars($this->request->getPost('username'), ENT_QUOTES, 'UTF-8');
+        $password = htmlspecialchars($this->request->getPost('password'), ENT_QUOTES, 'UTF-8');
+
+        if (!$userModel->validasiUserLogin($username, $password)) {
+            $this->session->setFlashdata('error', 'Username/Password Salah.');
+            return redirect()->to('masuk');
+        }
+
+        $user = $userModel->where('username', $username)->first();
+
+        $sessLogin = [
+            'isLogin' => true,
+            'role' => $user['role'],
+            'username' => $user['username'],
+            'nama' => $user['nama']
+        ];
+
+        $this->session->set($sessLogin);
+
+        if ($user['role'] === 'seller') {
+            $this->session->setFlashdata('sukses', 'Selamat Datang Seller!');
+            return redirect()->to('seller');
+        }
+
+        return redirect()->to('/');
+
+    }
+
+    public function prosesRegister() {
+        if ($this->request->getMethod() !== 'post') {
+            throw \CodeIgniter\Exceptions\PageNotFoundException::forPageNotFound();
+        }
+
+        $data = $this->request->getPost();
+
+        $data['username'] = htmlspecialchars($data['username'], ENT_QUOTES, 'UTF-8');
+        $data['email'] = htmlspecialchars($data['email'], ENT_QUOTES, 'UTF-8');
+        $data['nomorhp'] = htmlspecialchars($data['nomorhp'], ENT_QUOTES, 'UTF-8');
+        $data['password'] = htmlspecialchars($data['password'], ENT_QUOTES, 'UTF-8');
+        $data['confirmpassword'] = htmlspecialchars($data['confirmpassword'], ENT_QUOTES, 'UTF-8');
+        
+        if (empty($data['username']) || empty($data['email']) || empty($data['nomorhp']) || empty($data['password']) || empty($data['confirmpassword'])) {
+            $this->session->setFlashdata('error', 'Mohon Lengkapi Data!');
+            return redirect()->to('register');
+        }
+
+        if ($data['password'] !== $data['confirmpassword']) {
+            $this->session->setFlashdata('error', 'Password tidak sama!');
+            return redirect()->to('register');
+        }
+
+        $userModel = new UserModel();
+        $userModel->register($data);
+
+        
     }
 }
---
+++ lapaksiswa/app/Models/UserModel.php
@@ -0,0 +1,40 @@
+<?php 
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class UserModel extends Model {
+    protected $table = 'user';
+    protected $primaryKey = 'id';
+    protected $allowedFields = [
+        'user_id',
+        'username',
+        'nama',
+        'tanggal_lahir',
+        'email',
+        'no_hp',
+        'role',
+        'foto',
+        'alamat',
+        'kelas',
+        'password',
+        'token_reset_password',
+        'last_online'
+    ];
+
+    public function validasiUserLogin($username, $password) {
+        $user = $this->where('username', $username)->first();
+
+        if (!$user) {
+            return false;
+        }
+
+        return password_verify($password, $user['password']);
+
+    }
+
+    public function register($data) {
+        $this->insert($data);
+    }
+}
